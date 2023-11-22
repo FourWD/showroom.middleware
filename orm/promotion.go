af137fb8577865d9a20390bf64865b7ba6a689d5
Narongrit Mekpho <narongritjamenrtmp@gmail.com>
2023-11-22 18:59:36 +0700

    update commit

--- orm/promotion.go
+++
@@ -1,9 +0,0 @@
-package orm
-
-// midOrm "github.com/FourWD/middleware/orm"
-
-type Promotion struct {
-	ID string `json:"id" query:"id" gorm:"type:varchar(36)"`
-
-	Name string `json:"name" query:"name" gorm:"type:varchar(255)"`
-}
