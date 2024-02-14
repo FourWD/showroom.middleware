e5c97e7286420141e95fe58b31f25f880c232dbc
kamontip <bangwan.ap@hotmail.com>
2024-02-14 17:01:18 +0700

    add id for Users

--- orm/users.go
+++ orm/users.go
@@ -1,6 +1,7 @@
 package orm
 
 type Users struct {
+	ID       string `json:"id" query:"id" gorm:"type:varchar(36)"`
 	Username string `json:"username" query:"username" gorm:"type:varchar(20)"`
 	Password string `json:"password" query:"password" gorm:"type:varchar(20)"`
 }
