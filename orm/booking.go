4def2ae23dafbbcc57bad7c4ed081277017498d9
kamontip <bangwan.ap@hotmail.com>
2024-02-15 11:27:10 +0700

    test create booking

--- orm/booking.go
+++ orm/booking.go
@@ -1,9 +1,13 @@
 package orm
 
 import (
-	midOrm "github.com/FourWD/middleware/orm"
+	"github.com/FourWD/middleware/model"
 )
 
 type Booking struct { ///
-	midOrm.Booking //
+	// midOrm.Booking //
+	ID string `json:"id" query:"id" gorm:"type:varchar(36); uniqueIndex:idx_id "` //
+	model.GormModel
+	BookingNo       string `json:"booking_no" query:"booking_no" gorm:"type:varchar(20)"` //หมายเลขรายการ
+	BookingStatusID string `json:"booking_status_id" query:"booking_status_id" gorm:"type:varchar(2)"`
 }
