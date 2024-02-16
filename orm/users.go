d5b414ed12c0635b93f63804f6e5368c7ea68cd4
kamontip <bangwan.ap@hotmail.com>
2024-02-16 10:40:34 +0700

    [Adjust] user's password length

--- orm/users.go
+++ orm/users.go
@@ -7,7 +7,7 @@ type Users struct {
 	Firstname    string `json:"firstname" query:"firstname" gorm:"type:varchar(100)"`
 	Lastname     string `json:"lastname" query:"lastname" gorm:"type:varchar(100)"`
 	Username     string `json:"username" query:"username" gorm:"type:varchar(20)"`
-	Password     string `json:"password" query:"password" gorm:"type:varchar(30)"`
+	Password     string `json:"password" query:"password" gorm:"text"`
 	FileAvatarID string `json:"file_avartar_id" query:"file_avartar_id" gorm:"type:varchar(36)"`
 	Mobile       string `json:"mobile" query:"mobile" gorm:"type:varchar(10);unique"`
 	Email        string `json:"email" query:"email" gorm:"type:varchar(50)"`
