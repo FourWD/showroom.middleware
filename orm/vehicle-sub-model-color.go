1888af0f3b5def4148d5315d26dd7fd96e1f67ce
fourwd company <fourwdcompany@fourwds-Mac-mini-2.local>
2024-02-19 02:57:00 -0800

    update yearmunufacturing string to int

--- orm/vehicle-sub-model-color.go
+++ orm/vehicle-sub-model-color.go
@@ -8,7 +8,7 @@ type VehicleSubModelColors struct {
 
 	VehicleSubModelID string `json:"vehicle_sub_model_id" query:"vehicle_sub_model_id" gorm:"type:varchar(36)"`
 	VehicleColorID    string `json:"vehicle_color_id" query:"vehicle_color_id" gorm:"type:varchar(36)"`
-	YearManufacturing string `json:"year_manufacturing" query:"year_manufacturing" gorm:"type:varchar(4)"`
+	YearManufacturing int    `json:"year_manufacturing" query:"year_manufacturing" gorm:"type:varchar(4)"`
 
 	model.GormRowOrder
 }
