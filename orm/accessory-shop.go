bd375806d709c7ee8aab37ad81a3aadbea2b619a
suradevv <opapayao1@hotmail.com>
2024-02-23 17:31:26 +0700

    change name shop to accessory_shop

--- orm/accessory-shop.go
+++ orm/accessory-shop.go
@@ -2,7 +2,7 @@ package orm
 
 import "github.com/FourWD/middleware/model"
 
-type Shop struct {
+type AccessoryShop struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);uniqueIndex:idx_id;primary_key"`
 	model.GormModel
 
