6d30667a8150bead0ddcd80f493b62698e9e2509
suradevv <opapayao1@hotmail.com>
2024-02-27 17:02:51 +0700

    update shop_id in temp-booking-accessory

--- orm/temp-booking-accessory.go
+++ orm/temp-booking-accessory.go
@@ -5,4 +5,5 @@ import "github.com/FourWD/middleware/orm"
 type TempBookingAccessory struct {
 	orm.BookingAccessory
 	GroupID string `json:"group_id" query:"group_id" gorm:"type:varchar(2)"`
+	ShopID  string `json:"shop_id" query:"shop_id" gorm:"type:varchar(36)"`
 }
