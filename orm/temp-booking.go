f5a877917db9654c58d8dbe1a2eba15ccaab21fd
suradevv <opapayao1@hotmail.com>
2024-02-29 17:42:37 +0700

    update temp-booking

--- orm/temp-booking.go
+++ orm/temp-booking.go
@@ -1,8 +1,17 @@
 package orm
 
-import "github.com/FourWD/middleware/orm"
+import (
+	"time"
+
+	"github.com/FourWD/middleware/orm"
+)
 
 type TempBooking struct {
 	orm.Booking
-	VehicleYear string `json:"vehicle_year" query:"vehicle_year" gorm:"type:varchar(4)"`
+	VehicleYear      string    `json:"vehicle_year" query:"vehicle_year" gorm:"type:varchar(4)"`
+	IDCard           string    `json:"id_card" query:"id_card" gorm:"type:varchar(13)"`
+	BirthDate        time.Time `json:"birth_date" query:"birth_date"`
+	Occupation       string    `json:"occupation" query:"occupation" gorm:"type:varchar(255)"`
+	Telephone        string    `json:"telephone" query:"telephone" gorm:"type:varchar(30)"`
+	CustomerSourceID string    `json:"customer_source_id" query:"customer_source_id" gorm:"type:varchar(36)"`
 }
