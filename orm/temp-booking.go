62ce194a991179cf5791ed8434dc1592d1f52a65
suradevv <opapayao1@hotmail.com>
2024-03-05 15:33:37 +0700

    update temp-booking

--- orm/temp-booking.go
+++ orm/temp-booking.go
@@ -10,6 +10,7 @@ type TempBooking struct {
 	orm.Booking
 	VehicleYear      string    `json:"vehicle_year" query:"vehicle_year" gorm:"type:varchar(4)"`
 	IDCard           string    `json:"id_card" query:"id_card" gorm:"type:varchar(13)"`
+	JuristicID       string    `json:"juristic_id" query:"juristic_id" gorm:"type:varchar(13)"`
 	BirthDate        time.Time `json:"birth_date" query:"birth_date"`
 	Occupation       string    `json:"occupation" query:"occupation" gorm:"type:varchar(255)"`
 	Telephone        string    `json:"telephone" query:"telephone" gorm:"type:varchar(30)"`
