d7d07dfe23675233aa158928f2545dc52ebe0dce
suradevv <opapayao1@hotmail.com>
2024-03-06 11:50:50 +0700

    update birth_date in temp-booking

--- orm/temp-booking.go
+++ orm/temp-booking.go
@@ -11,7 +11,7 @@ type TempBooking struct {
 	VehicleYear      string    `json:"vehicle_year" query:"vehicle_year" gorm:"type:varchar(4)"`
 	IDCard           string    `json:"id_card" query:"id_card" gorm:"type:varchar(13)"`
 	JuristicID       string    `json:"juristic_id" query:"juristic_id" gorm:"type:varchar(13)"`
-	BirthDate        time.Time `json:"birth_date" query:"birth_date"`
+	BirthDate        time.Time `json:"birth_date" query:"birth_date" gorm:"type:date"`
 	Occupation       string    `json:"occupation" query:"occupation" gorm:"type:varchar(255)"`
 	Telephone        string    `json:"telephone" query:"telephone" gorm:"type:varchar(30)"`
 	CustomerSourceID string    `json:"customer_source_id" query:"customer_source_id" gorm:"type:varchar(36)"`
