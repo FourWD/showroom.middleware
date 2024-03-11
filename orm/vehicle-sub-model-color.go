e000fd5aa780cdc30410fdae6c926cabfdb70638
fourwd company <fourwdcompany@fourwds-Mac-mini-2.local>
2024-03-11 02:10:03 -0700

    update vehicle-sub-model-color

--- orm/vehicle-sub-model-color.go
+++ orm/vehicle-sub-model-color.go
@@ -6,8 +6,10 @@ type VehicleSubModelColors struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);uniqueIndex:idx_id;primary_key"`
 	model.GormModel
 
-	VehicleSubModelID string `json:"vehicle_sub_model_id" query:"vehicle_sub_model_id" gorm:"type:varchar(36)"`
-	VehicleColorID    string `json:"vehicle_color_id" query:"vehicle_color_id" gorm:"type:varchar(36)"`
-	YearManufacturing int    `json:"year_manufacturing" query:"year_manufacturing" gorm:"type:varchar(4)"`
+	VehicleSubModelID string  `json:"vehicle_sub_model_id" query:"vehicle_sub_model_id" gorm:"type:varchar(36)"`
+	VehicleColorID    string  `json:"vehicle_color_id" query:"vehicle_color_id" gorm:"type:varchar(36)"`
+	YearManufacturing int     `json:"year_manufacturing" query:"year_manufacturing" gorm:"type:varchar(4)"`
+	Price             float64 `json:"price" query:"price" gorm:"type:decimal(14,2)"`
+
 	model.GormRowOrder
 }
