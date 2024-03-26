d636c0ee945f25979a96407e972c91b38cfa000a
suradevv <opapayao1@hotmail.com>
2024-03-26 17:33:41 +0700

    update finance-interest

--- orm/finance-interest.go
+++ orm/finance-interest.go
@@ -5,7 +5,7 @@ import (
 )
 
 type FinanceInterest struct {
-	midOrm.Insurance
+	midOrm.FinanceInterest
 	VehicleModelID     string `json:"vehicle_model_id" query:"vehicle_model_id" gorm:"type:varchar(36)"`
 	IsIncludeInsurance bool   `json:"is_include_insurance" query:"is_include_insurance" gorm:"type:bool"`
 }
