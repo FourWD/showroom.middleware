43e7fdf0b5bd6632cbf44a2f0f2a060ae939a562
suradevv <opapayao1@hotmail.com>
2024-04-04 16:52:55 +0700

    update datetime signature_date in bookings

--- orm/booking.go
+++ orm/booking.go
@@ -8,20 +8,21 @@ import (
 
 type Booking struct {
 	midOrm.Booking
-	VehicleYear                     string    `json:"vehicle_year" query:"vehicle_year" gorm:"type:varchar(4)"`
-	IDCard                          string    `json:"id_card" query:"id_card" gorm:"type:varchar(13)"`
-	CompanyRegisterNo               string    `json:"company_register_no" query:"company_register_no" gorm:"type:varchar(13)"`
-	BirthDate                       time.Time `json:"birth_date" query:"birth_date" gorm:"type:date"`
-	OccupationID                    string    `json:"occupation_id" query:"occupation_id" gorm:"type:varchar(255)"`
-	Telephone                       string    `json:"telephone" query:"telephone" gorm:"type:varchar(30)"`
-	CustomerSourceID                string    `json:"customer_source_id" query:"customer_source_id" gorm:"type:varchar(36)"`
-	CustomerTypeID                  string    `json:"customer_type_id" query:"customer_type_id" gorm:"type:varchar(2)"`
-	CustomerSignature               string    `json:"customer_signature" query:"customer_signature" gorm:"type:varchar(500)"`
-	CustomerSignatureDate           time.Time `json:"customer_signature_date" query:"customer_signature_date" gorm:"type:date"`
-	EmployeeSignature               string    `json:"employee_signature" query:"employee_signature" gorm:"type:varchar(500)"`
-	EmployeeSignatureDate           time.Time `json:"employee_signature_date" query:"employee_signature_date" gorm:"type:date"`
-	ManagerSignature                string    `json:"manager_signature" query:"manager_signature" gorm:"type:varchar(500)"`
-	ManagerSignatureDate            time.Time `json:"manager_signature_date" query:"manager_signature_date" gorm:"type:date"`
+	VehicleYear           string    `json:"vehicle_year" query:"vehicle_year" gorm:"type:varchar(4)"`
+	IDCard                string    `json:"id_card" query:"id_card" gorm:"type:varchar(13)"`
+	CompanyRegisterNo     string    `json:"company_register_no" query:"company_register_no" gorm:"type:varchar(13)"`
+	BirthDate             time.Time `json:"birth_date" query:"birth_date" gorm:"type:date"`
+	OccupationID          string    `json:"occupation_id" query:"occupation_id" gorm:"type:varchar(255)"`
+	Telephone             string    `json:"telephone" query:"telephone" gorm:"type:varchar(30)"`
+	CustomerSourceID      string    `json:"customer_source_id" query:"customer_source_id" gorm:"type:varchar(36)"`
+	CustomerTypeID        string    `json:"customer_type_id" query:"customer_type_id" gorm:"type:varchar(2)"`
+	CustomerSignature     string    `json:"customer_signature" query:"customer_signature" gorm:"type:varchar(500)"`
+	CustomerSignatureDate time.Time `json:"customer_signature_date" query:"customer_signature_date"`
+	EmployeeSignature     string    `json:"employee_signature" query:"employee_signature" gorm:"type:varchar(500)"`
+	EmployeeSignatureDate time.Time `json:"employee_signature_date" query:"employee_signature_date"`
+	ManagerSignature      string    `json:"manager_signature" query:"manager_signature" gorm:"type:varchar(500)"`
+	ManagerSignatureDate  time.Time `json:"manager_signature_date" query:"manager_signature_date"`
+
 	EmployeeID                      string    `json:"employee_id" query:"employee_id" gorm:"type:varchar(36)"`
 	ManagerID                       string    `json:"manager_id" query:"manager_id" gorm:"type:varchar(36)"`
 	IsConsent                       bool      `json:"is_consent" query:"is_consent" gorm:"type:bool"`
