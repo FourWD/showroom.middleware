cb8bac12a889614ca90b88959519bd64b4052e3c
fourwd company <fourwdcompany@fourwds-Mac-mini-2.local>
2024-04-28 21:16:45 -0700

    update customer-source customer_source_id

--- orm/customer-source.go
+++ orm/customer-source.go
@@ -6,7 +6,8 @@ type CustomerSource struct {
 	ID string `json:"id" query:"id" gorm:"type:varchar(36);uniqueIndex:idx_id;primary_key"`
 	model.GormModel
 
-	Name string `json:"name" query:"name" gorm:"type:varchar(200)"`
+	Name             string `json:"name" query:"name" gorm:"type:varchar(200)"`
+	CustomerSourceID string `json:"customer_source_id" query:"customer_source_id" gorm:"type:varchar(36)"`
 
 	model.GormRowOrder
 }
