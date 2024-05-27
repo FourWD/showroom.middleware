32d727bd951cbfc94493a04ced84a9748bf8c2c5
fourwd company <fourwdcompany@fourwds-Mac-mini-2.local>
2024-05-27 02:32:08 -0700

    update vehicle-model start_price

--- orm/vehicle-model.go
+++ orm/vehicle-model.go
@@ -6,5 +6,5 @@ import (
 
 type VehicleModel struct {
 	midOrm.VehicleModel
-	StartPrice int `json:"start_price" query:"start_price" gorm:"type:int"`
+	StartPrice int `json:"start_price" query:"start_price" gorm:"type:decimal(14,2)"`
 }
